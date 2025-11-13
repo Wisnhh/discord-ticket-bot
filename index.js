7c7ebfb944232a3fdeb56475ac70820b523ae40d
Doughlas <88969063+Wisnhh@users.noreply.github.com>
2025-11-13 08:38:45 +0700

    index.js

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
+import mongoose from "mongoose";
 import keepAlive from "./keep_alive.js";
 keepAlive();
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => console.log("✅ MongoDB Connected"))
+.catch(err => console.error("❌ MongoDB Connection Error:", err));
 process.on("unhandledRejection", (error) => {
   console.error("Unhandled promise rejection:", error);
 });

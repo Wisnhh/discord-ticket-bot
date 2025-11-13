e306ca77a03df76fc96b5fd7c0697ef9f7860493
Doughlas <88969063+Wisnhh@users.noreply.github.com>
2025-11-13 16:55:37 +0700

    guildMemberAdd.js

---
+++ events/guildMemberAdd.js
@@ -0,0 +1,24 @@
+import Invite from "../models/invite.js";
+
+export default async (client, member) => {
+  const newInvites = await member.guild.invites.fetch();
+  const cachedInvites = client.invites.get(member.guild.id);
+
+  const usedInvite = newInvites.find(inv => cachedInvites.get(inv.code) < inv.uses);
+
+  if (usedInvite) {
+    await Invite.findOneAndUpdate(
+      { code: usedInvite.code },
+      {
+        guildId: member.guild.id,
+        inviterId: usedInvite.inviter?.id || "unknown",
+        uses: usedInvite.uses,
+      },
+      { upsert: true }
+    );
+
+    console.log(`✅ Invite ${usedInvite.code} milik ${usedInvite.inviter?.tag} digunakan.`);
+  }
+
+  client.invites.set(member.guild.id, new Map(newInvites.map(i => [i.code, i.uses])));
+};
